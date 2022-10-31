7b9b969a5131a0a1278484db75fcace44ad089a8
Yash Wardhan <56475291+Yash-wardhan@users.noreply.github.com>
2022-10-31 17:17:20 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyDFzgzxTpGJWCzVDG5mVJAXsujUx-2Lbzw",
+    authDomain: "linkdin-clone-12641.firebaseapp.com",
+    projectId: "linkdin-clone-12641",
+    storageBucket: "linkdin-clone-12641.appspot.com",
+    messagingSenderId: "248909297440",
+    appId: "1:248909297440:web:81c08a58c63a53afe7c945",
+    measurementId: "G-F5SGCS15WT"
+  };
